c98274ec230de1b76594a348420891deb902881f
matis <matis.baguelin@gmail.com>
2023-02-14 12:16:20 +0100

    loading animation

--- src/components/SearchBarAutocomplete.tsx
+++ src/components/SearchBarAutocomplete.tsx
@@ -9,6 +9,7 @@ declare const process: {
 
 function SearchBarAutocomplete() {
   const [value, setValue] = useState<string>('')
+  const [loading, setLoading] = useState<boolean>(false)
   const [results, setResults] = useState<any[]>([])
   const [showResults, setShowResults] = useState<boolean>(false) // TODO: Typer dans un fichier utils
   const [clicked, setClicked] = useState(false)
@@ -31,6 +32,8 @@ function SearchBarAutocomplete() {
       const API_KEY: string = process.env.REACT_APP_RAPID_API_KEY
       // Faire un fichier utils ?
 
+      setLoading(true)
+
       fetch(
         `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${debouncedValue}&minPopulation=20000&types=City`,
         {
@@ -48,6 +51,7 @@ function SearchBarAutocomplete() {
         .then((res) => {
           setResults(res.data)
           setShowResults(true)
+          setLoading(false)
           console.log(res.data)
         })
       // TODO: gérer les erreurs
@@ -59,13 +63,37 @@ function SearchBarAutocomplete() {
 
   return (
     <div className="flex flex-col w-full mb-4">
-      <input
-        value={value}
-        onChange={(e) => handleInputChange(e.target.value)}
-        type="search"
-        className="flex-1 px-3 py-1.5 min-w-0 text-gray-700 bg-white border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-        placeholder="Search"
-      />
+      <div className="flex items-center relative">
+        <input
+          value={value}
+          onChange={(e) => handleInputChange(e.target.value)}
+          type="text"
+          className="flex-1 px-3 py-1.5 min-w-0 text-gray-700 bg-white border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+          placeholder="Search"
+        />
+        {loading && (
+          <svg
+            className="absolute right-2.5 bottom-2.5 animate-spin ml-3 h-5 w-5 text-blue-600"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        )}
+      </div>
       {showResults && (
         <ul className="w-full bg-white border shadow-sm rounded">
           {results.map((result) => (
